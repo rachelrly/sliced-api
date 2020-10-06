1e97031815dab8fc3e9c9d2d3d02864ad9b693a8
Rachel Reilly <r.a.reill18@gmail.com>
2020-10-06 17:20:56 -0500

    again

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
+    "ssl": true,
     "connectionString": (process.env.NODE_ENV === 'test')
         ? process.env.TEST_DATABASE_URL
         : process.env.DATABASE_URL,
